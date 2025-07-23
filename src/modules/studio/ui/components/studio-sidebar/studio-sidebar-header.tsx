2af1dd73f97e03a3306b20551fbaab5dbf03a1af
SMT310 <shinizeam@gmail.com>
2025-07-24 05:03:48 +0700

    Complete studio topnav sidebar

---
+++ src/modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -0,0 +1,69 @@
+import {
+  SidebarHeader,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import UserAvatar from "@/components/user-avatar";
+// import { useUser } from "@clerk/nextjs"
+import Link from "next/link";
+
+export const StudioSidebarHeader = () => {
+  // const { user } = useUser();
+  const { state } = useSidebar();
+  // if (!user) return (
+  //     <SidebarHeader className="flex items-center justify-center pb-4">
+  //         <Skeleton className="size-[112px] rounded-full" />
+  //         <div className="flex flex-col items-center mt-2 gap-y-2">
+  //             <Skeleton className="w-[80px] h-4" />
+  //             <Skeleton className="w-[100px] h-4" />
+  //         </div>
+  //     </SidebarHeader>
+  // )
+
+  if (state === "collapsed") {
+    return (
+      <SidebarMenuItem>
+        <SidebarMenuButton
+          tooltip="Your profile"
+          asChild
+        >
+          <Link
+            prefetch
+            href="/users/current"
+          >
+            <UserAvatar
+              imageUrl="https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-2210.jpg?semt=ais_hybrid&w=740"
+              name="User"
+              size="xl"
+            />
+            <span className="text-sm">Your profile</span>
+          </Link>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    );
+  }
+
+  return (
+    <SidebarHeader className="flex items-center justify-center pb-4">
+      <Link
+        prefetch
+        href="/users/current"
+      >
+        {/* <UserAvatar imageUrl={user?.imageUrl ?? ""} name={user?.fullName ?? "User"}
+                    className="size-[112px] hover:opacity-80 transition-opacity"
+                /> */}
+        <UserAvatar
+          imageUrl="https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-gender-neutral-silhouette-profile-picture-suitable-social-media-profiles-icons-screensavers-as-templatex9xa_719432-2210.jpg?semt=ais_hybrid&w=740"
+          name="User"
+          size="xl"
+        />
+      </Link>
+      <div className="flex flex-col items-center mt-2 gap-y-2">
+        <p className="text-sm font-medium">Your profile</p>
+        {/* <p className="text-xs text-muted-foreground">{user.fullName}</p> */}
+      </div>
+    </SidebarHeader>
+  );
+};
