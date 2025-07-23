fa4750810b7879757d81b8c78b4b917c0be85f8b
Tập Làm Dev <dangtuanhung1692003@gmail.com>
2025-07-24 03:21:16 +0700

    Complete the sidebar-topnav

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,23 +1,15 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import  HomeNavbar  from "@/modules/home/ui/components/home-navbar";
 import HomeLayout from "@/modules/home/ui/layouts/home-layout";
+
+export const dynamic = "force-dynamic";
+
 interface LayoutProps {
     children: React.ReactNode;
-};
-
-const Layout = ({ children }: LayoutProps) => {
-    return (
-        <SidebarProvider>
-            <div className="w-full">
-                <HomeNavbar/>
-                <HomeLayout>
-                    {children}
-                </HomeLayout>
-            </div>
-
-        </SidebarProvider>
-
-    )
 }
 
-export default Layout
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <HomeLayout>
+            {children}
+        </HomeLayout>
+    )
+}
---
+++ src/modules/home/ui/components/home-sidebar/otherservices-section.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { pageUrls } from "@/lib/enums/page-urls";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { Crown, ListMusic , Clapperboard, CircleUserRound   } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const items = [
+    {
+        title: "WeTube Premium",
+        url: pageUrls.HISTORY,
+        icon: Crown ,
+    },
+    {
+        title: "WeTube Studio",
+        url: pageUrls.LIKED_VIDEOS,
+        icon: Clapperboard ,
+    },
+    {
+        title: "WeTube Music",
+        url: pageUrls.ALL_PLAYLISTS,
+        icon: ListMusic 
+    },
+    {
+        title: "WeTube Kids",
+        url: pageUrls.YOURVIDEO,
+        icon: CircleUserRound  ,
+        auth: true
+    }
+];
+
+export const OtherServices = () => {
+    const { isSignedIn } = useAuth();
+    const clerk = useClerk();
+    const pathname = usePathname();
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Other services of WeTube</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item, index) => (
+                        <SidebarMenuItem key={index}>
+                            <SidebarMenuButton
+                                tooltip={item.title}
+                                asChild
+                                isActive={pathname === item.url}
+                                onClick={(e) => {
+                                    if (!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        return clerk.openSignIn();
+                                    }
+                                }}
+                            >
+                                <Link prefetch href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
