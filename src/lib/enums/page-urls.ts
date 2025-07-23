aaddbbfd566cc9d6fdcbc5089b9c25e5660dbf63
SMT310 <shinizeam@gmail.com>
2025-07-23 13:28:49 +0700

    first commit

---
+++ src/lib/enums/page-urls.ts
@@ -0,0 +1,9 @@
+export const pageUrls = {
+    HOME: "/",
+    SUBSCRIPTIONS: "/feed/subscribed",
+    TRENDING: "/feed/trending",
+    HISTORY: "/playlists/history",
+    LIKED_VIDEOS: "/playlists/liked",
+    ALL_PLAYLISTS: "/playlists",
+    STUDIO: "/studio"
+} as const;
