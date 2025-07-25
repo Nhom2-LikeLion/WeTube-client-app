1b262dfdf3277b5f1001e61c82ead91a9cd8198c
SMT310 <shinizeam@gmail.com>
2025-07-25 13:53:14 +0700

    Fix music layout

--- src/modules/music/ui/layouts/music-layout.tsx
+++ src/modules/music/ui/layouts/music-layout.tsx
@@ -1,8 +1,4 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import Sidebar from "../components/music-sidebar";
-import MusicSidebar from "../components/music-sidebar";
-import HomeNavbar from "@/modules/home/ui/components/home-navbar";
-import MusicNavbar from "../components/music-navbar";
+import Music from "../components";
 
 
 interface MusicLayoutProps {
@@ -11,18 +7,6 @@ interface MusicLayoutProps {
 
 export default function MusicLayout({ children }: MusicLayoutProps) {
     return (
-        <SidebarProvider>
-            <div className="w-full">
-                <MusicNavbar/>
-                <div className="flex min-h-screen">
-                    <MusicSidebar />
-                    <main className="flex-1 overflow-y-auto">
-                        {children}
-                    </main>
-                </div>
-            </div>
-
-        </SidebarProvider>
-
-    )
+        <Music/>
+    );
 }
