d7a3d9b82d562bbf7ccd2db4227c040e2284f720
TaiTran <avetai2k3@gmail.com>
2025-09-17 15:03:11 +0700

    "add"

--- src/app/(home)/playlists/liked/page.tsx
+++ src/app/(home)/playlists/liked/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Watchlaterview from '@/modules/playlists/ui/view/watchlater-view';
+import Likedview from '@/modules/playlists/ui/view/liked-view';
 import React from 'react'
 
 export default function watchlater() {
   return (
-    <Watchlaterview Params={{playlistId:"c34d0c63-ab6b-48f8-90a7-bd359df845e6"}}/>
+    <Likedview Params={{playlistId:"c34d0c63-ab6b-48f8-90a7-bd359df845e6"}}/>
   )
 }
