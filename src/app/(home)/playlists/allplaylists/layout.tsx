3f29c2c13dd5f7c00463727f9911913cc0766ef8
SMT310 <shinizeam@gmail.com>
2025-09-24 01:15:32 +0700

    Fix n times for deploy

--- src/app/(home)/playlists/allplaylists/layout.tsx
+++ src/app/(home)/playlists/allplaylists/layout.tsx
@@ -1,5 +1,5 @@
 import ProtectedRoute from '@/components/ProtectedRoute';
-import Allplaylistlayout from "@/modules/playlists/layouts/allplaylist-layout";
+import Allplaylistlayout from "@/modules/playlists/layouts/Allplaylist-layout";
 
 export const dynamic = "force-dynamic";
 
