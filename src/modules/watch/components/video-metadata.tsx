0dcb54eb24bdb702a67861c3a7ead0cfcf15a9ea
soonsangmin <soonsangmin2831@gmail.com>
2025-09-23 16:06:31 +0700

    update

--- src/modules/watch/components/video-metadata.tsx
+++ src/modules/watch/components/video-metadata.tsx
@@ -2,86 +2,104 @@
 
 import Image from "next/image";
 import {
-  BellIcon,
-  ChevronDownIcon,
-  EllipsisHorizontalIcon,
-  HandThumbDownIcon,
-  HandThumbUpIcon,
-  HeartIcon,
+    BellIcon,
+    ChevronDownIcon,
+    EllipsisHorizontalIcon,
+    HandThumbDownIcon,
+    HandThumbUpIcon,
+    HeartIcon,
 } from "@heroicons/react/24/outline";
-import { useVideoStore } from "@/store/zustand/videoStore";
-import { timeAgo } from "@/lib/utils";
+import {useVideoStore} from "@/store/zustand/videoStore";
+import {timeAgo} from "@/lib/utils";
+import { useState } from "react";
 
 export default function VideoMetadata() {
-  const videoDetail = useVideoStore((state) => state.videoDetail);
+    const videoDetail = useVideoStore((state) => state.videoDetail);
 
-  if (!videoDetail) return null;
+    if (!videoDetail) return null;
 
-  const { title, description, totalView, createAt, name, picture, totalSubscribers } =
-      videoDetail.detail;
+    const {title, description, totalView, createAt, name, picture, totalSubscribers, subscribed} =
+        videoDetail.detail;
 
-  return (
-      <section className="w-full mt-2">
-        {/* Title */}
-        <p className="text-2xl font-bold tracking-tight">{title}</p>
+    const [isSubscribed, setIsSubscribed] = useState(subscribed);
 
-        <div className="py-1 flex justify-between items-center mt-2">
-          <section className="flex justify-start items-center gap-3">
-            <Image
-                className="rounded-full h-10 w-10 cursor-pointer"
-                src={picture}
-                alt={name}
-                height={40}
-                width={40}
-            />
-            <div className="hidden md:block">
-              <section className="flex flex-col tracking-tight mr-4">
-                <p className="font-bold font-lg cursor-pointer">{name}</p>
+    return (
+        <section className="w-full mt-2">
+            {/* Title */}
+            <p className="text-2xl font-bold tracking-tight">{title}</p>
 
-                 <p className="text-gray-500 text-xs font-semibold">{totalSubscribers}</p>
-              </section>
+            <div className="py-1 flex justify-between items-center mt-2">
+                <section className="flex justify-start items-center gap-3">
+                    <Image
+                        className="rounded-full h-10 w-10 cursor-pointer"
+                        src={picture}
+                        alt={name}
+                        height={40}
+                        width={40}
+                    />
+                    <div className="hidden md:block">
+                        <section className="flex flex-col tracking-tight mr-4">
+                            <p className="font-bold font-lg cursor-pointer">{name}</p>
+
+                            <p className="text-gray-500 text-xs font-semibold">{totalSubscribers}</p>
+                        </section>
+                    </div>
+                    {!isSubscribed ? (
+                        <button
+                            onClick={() => setIsSubscribed(true)}
+                            className="rounded-full bg-black text-white hover:bg-gray-800 py-2 px-4 font-bold text-sm cursor-pointer"
+                        >
+                            Subscribe
+                        </button>
+                    ) : (
+                        <button
+                            onClick={() => setIsSubscribed(false)}
+                            className="rounded-full flex gap-2 bg-gray-100 hover:bg-gray-200 py-2 px-3 cursor-pointer items-center"
+                        >
+                            <BellIcon className="size-5" />
+                            <p className="text-sm font-bold">Subscribed</p>
+                            <ChevronDownIcon className="size-4.5" />
+                        </button>
+                    )}
+                </section>
+
+                <section className="flex justify-end items-center gap-3 ">
+                    <div className="cursor-pointer flex justify-between bg-gray-100 rounded-full overflow-hidden">
+                        <button
+                            className="flex gap-2 py-2 px-3 bg-gray-100 hover:bg-gray-200 rounded-l-full cursor-pointer">
+                            <HandThumbUpIcon className="size-5"/>
+                            <p className="text-sm font-bold">17K</p>
+                        </button>
+                        <div className="border-l-[1px] border-gray-300 my-2"/>
+                        <button className="py-2 px-3 bg-gray-100 hover:bg-gray-200 rounded-r-full cursor-pointer">
+                            <HandThumbDownIcon className="size-5"/>
+                        </button>
+                    </div>
+                    <button
+                        className="bg-gray-100 hover:bg-gray-200 p-2 rounded-full cursor-pointer flex gap-2 py-2 px-2.5">
+                        <p className="text-sm font-bold">Share</p>
+                    </button>
+                    <div className="hidden md:block">
+                        <button
+                            className="bg-gray-100 hover:bg-gray-200 p-2 rounded-full cursor-pointer flex gap-2 py-2 px-2.5">
+                            <HeartIcon className="size-5"/>
+                            <p className="text-sm font-bold">Thanks</p>
+                        </button>
+                    </div>
+                    <button className="bg-gray-100 hover:bg-gray-200 p-2 rounded-full cursor-pointer">
+                        <EllipsisHorizontalIcon className="size-6"/>
+                    </button>
+                </section>
             </div>
-            <button className="rounded-full flex gap-2 bg-gray-100 hover:bg-gray-200 py-2 px-2.5 cursor-pointer">
-              <BellIcon className="size-5" />
-              <p className="text-sm font-bold">Subscribed</p>
-              <ChevronDownIcon className="size-4.5" />
-            </button>
-          </section>
 
-          <section className="flex justify-end items-center gap-3 ">
-            <div className="cursor-pointer flex justify-between bg-gray-100 rounded-full overflow-hidden">
-              <button className="flex gap-2 py-2 px-3 bg-gray-100 hover:bg-gray-200 rounded-l-full cursor-pointer">
-                <HandThumbUpIcon className="size-5" />
-                <p className="text-sm font-bold">17K</p>
-              </button>
-              <div className="border-l-[1px] border-gray-300 my-2" />
-              <button className="py-2 px-3 bg-gray-100 hover:bg-gray-200 rounded-r-full cursor-pointer">
-                <HandThumbDownIcon className="size-5" />
-              </button>
+            {/* Description */}
+            <div className="bg-gray-100 p-3 mt-2 w-full rounded-xl text-sm">
+                <p className="font-semibold">
+                    {totalView.toLocaleString()} views ·{" "}
+                    {timeAgo(createAt)}
+                </p>
+                <p>{description}</p>
             </div>
-            <button className="bg-gray-100 hover:bg-gray-200 p-2 rounded-full cursor-pointer flex gap-2 py-2 px-2.5">
-              <p className="text-sm font-bold">Share</p>
-            </button>
-            <div className="hidden md:block">
-              <button className="bg-gray-100 hover:bg-gray-200 p-2 rounded-full cursor-pointer flex gap-2 py-2 px-2.5">
-                <HeartIcon className="size-5" />
-                <p className="text-sm font-bold">Thanks</p>
-              </button>
-            </div>
-            <button className="bg-gray-100 hover:bg-gray-200 p-2 rounded-full cursor-pointer">
-              <EllipsisHorizontalIcon className="size-6" />
-            </button>
-          </section>
-        </div>
-
-        {/* Description */}
-        <div className="bg-gray-100 p-3 mt-2 w-full rounded-xl text-sm">
-          <p className="font-semibold">
-            {totalView.toLocaleString()} views ·{" "}
-            {timeAgo(createAt)}
-          </p>
-          <p>{description}</p>
-        </div>
-      </section>
-  );
+        </section>
+    );
 }
--- src/modules/watch/components/video-preview.tsx
+++ src/modules/watch/components/video-preview.tsx
@@ -1,19 +1,25 @@
-import { RelatedVideo } from "@/types/video";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { motion } from "motion/react";
+import { useRouter } from "next/navigation";
+import {RecommendVideoDto} from "@/types/video";
 
 const item = {
     hidden: { opacity: 0, scale: 0.9 },
     show: { opacity: 1, scale: 1 },
 };
 
-export default function VideoPreview({ video }: { video: RelatedVideo }) {
+export default function VideoPreview({ video }: { video: RecommendVideoDto }) {
+    const router = useRouter();
+    const handleClick = () => {
+        router.push(`/watch/${video.id}`);
+    };
+
     return (
         <motion.div variants={item} className="grid grid-cols-11 gap-2 mb-2">
             <div
                 className="col-span-5 rounded-lg bg-no-repeat bg-cover aspect-video relative cursor-pointer"
                 style={{ backgroundImage: `url(${video.thumbnailUrl})` }}
-            >
+                onClick={handleClick}>
                 <p
                     style={{ fontSize: "11px" }}
                     className="absolute right-1.5 bottom-1.5 rounded-sm bg-gray-800/85 text-white font-semibold px-1"
@@ -27,7 +33,7 @@ export default function VideoPreview({ video }: { video: RelatedVideo }) {
                 </p>
                 <span className="flex justify-start items-center gap-1">
           <p className="text-sm font-semibold text-gray-500">{video.name}</p>
-                    {/* giả sử tất cả đều verified */}
+
                     <CheckCircleIcon className="size-4 text-gray-500" />
         </span>
                 <p className="flex text-xs justify-start items-center gap-2">
